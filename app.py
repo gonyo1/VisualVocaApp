48a2a8e9a211a537607cf31c1971692d7b44fcba
Gonyo <sksgml1254@namsan-n.gwe.es.kr>
2023-09-19 17:20:29 +0900

    change get images part

--- app.py
+++ app.py
@@ -2,9 +2,12 @@
 import os.path
 import sys
 from glob import glob
+import time
+
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtWidgets import QMainWindow, QApplication, QFileDialog, QWidget
 from PyQt5.QtCore import QTimer
+
 from main_ui import Ui_MainApp as mp
 from resource.py import get_images
 
@@ -18,29 +21,29 @@ class MainWindow(QMainWindow, mp):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
         self.show()
-        self.setWindowTitle("  AutoSignature by Gonyo (Released 2022.12.19.)")
+        self.setWindowTitle("  VisualVoca by Gonyo (Released 2022.12.19.)")
         # self.setWindowIcon(QIcon(":/M1/icon.png"))
 
-        self.show_images()
-        self.mb_voca_word.itemClicked.connect(self.clicked)
+        self.word = None
+        self.mb_voca_word.itemClicked.connect(self.voca_change_title)
+        self.mb_voca_word.itemClicked.connect(self.voca_change_images)
 
-    def clicked(self):
-        word = self.mb_voca_word.currentItem().text()
-        self.mb_show_eng.setText(word)
-        get_images.get_images_from_word(word)
-        self.show_images()
+    def voca_change_title(self):
+        self.word = self.mb_voca_word.currentItem().text()
+        self.mb_show_eng.setText(self.word)
 
-    def show_images(self):
-        timer = QTimer(self)
-        timer.timeout.connect(self.next_image)
-        timer.start(3000)
-        self.pics = [QPixmap(item) for item in glob(f"./resource/img/jump/*.jpg")]
-        self.image_index = 0
+    def voca_change_images(self):
+        status = get_images.get_images_from_word(self.word)
+        print(status)
 
-    def next_image(self):
-        if self.image_index < len(self.pics):
-            self.mb_show_image.setPixmap(self.pics[self.image_index])
-            self.image_index += 1
+        self.pics = [QPixmap(item) for item in glob(f"./resource/img/{self.word}/*.jpg")]
+
+        for pic in self.pics:
+            self.mb_show_image.clear()
+            self.mb_show_image.setPixmap(pic)
+            time.sleep(2)
+            self.mb_show_image.repaint()
+
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
--- resource/py/get_images.py
+++ resource/py/get_images.py
@@ -15,37 +15,45 @@ except ModuleNotFoundError:
 # <--- Get Main Image src url from "GettyImage Web Site" --->
 
 def get_images_from_word(word: str = None) -> str:
+    def is_main_app(word: str = None) -> str:
+        base_dir = str()
+        if os.path.basename(os.path.abspath("./")) != "py":
+            base_dir = os.path.abspath(f"./resource/img/{word}")
+        else:
+            base_dir = os.path.abspath(f"../img/{word}")
+            
+        return base_dir
+
     def is_directory(path: str = None):
         if not os.path.isdir(path):
             os.mkdir(path)
 
-    def is_image_downloaded(path: str = None, word: str = None):
-        base_path = None
-        if os.path.basename(os.path.abspath("./")) != "py":
-            base_path = os.path.abspath(f"./resource/img/{word}")
+    def download_image_from_url(word: str = None):
+        # [repair] GettyImage 사이트에서 이미지 검색하는 URL로 사이트 업데이트에 대한 대응 필요한 부분
+        base_url = f"https://www.gettyimagesbank.com/s/?q={word}"
+        res = requests.get(base_url, verify=False)
+
+        if res.status_code == 200:
+            print("  Starting web site crawling...")
+            html = res.text
+            tree = lxml.html.fromstring(html)
+            selectors = tree.cssselect('#tiles > li > a > img')
+            src = [img.get("src") for img in selectors]
+            for idx, url in enumerate(src[:3]):
+                idx = str(idx + 1)
+                os.system(f"curl -k {url} > {base_dir}\\{word}({idx}).jpg")
+
+    def is_image_downloaded(path: str = None, word: str = None) -> str:
+        status = "waiting"
+        if os.path.isfile(os.path.abspath(f"{path}\\{word}(1).jpg")):
+            status = "saved"
         else:
-            base_path = os.path.abspath(f"../img/{word}")
-        is_directory(base_path)
-        os.path.isfile()
+            is_directory(path)
+            download_image_from_url(word)
+            status = "downloaded"
+        return status
 
-    def download_image_from_url(url: str = None, index: int = None, word: str = None):
-        idx = str(index + 1)
-        print(f"curl {url} > {word}({idx}).jpg")
-        os.system(f"curl -k {url} > {base_path}\\{word}({idx}).jpg")
+    base_dir = is_main_app(word=word)
+    status = is_image_downloaded(path=base_dir, word=word)
 
-    url = str()
-    keyword = word
-    base_url = f"https://www.gettyimagesbank.com/s/?q={keyword}"
-
-    res = requests.get(base_url, verify=False)
-
-    if res.status_code == 200:
-        print("  Starting web site crawling...")
-        html = res.text
-        tree = lxml.html.fromstring(html)
-        selectors = tree.cssselect('#tiles > li > a > img')
-        src = [img.get("src") for img in selectors]
-        for idx, url in enumerate(src[:3]):
-            download_image_from_url(url, idx, word)
-
-    return url
+    return status
