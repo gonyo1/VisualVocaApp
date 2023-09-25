db02accc6bf3ece2a2aad2883e04512b47006a90
Gonyo <121369699+gonyo1@users.noreply.github.com>
2023-09-25 17:23:17 +0900

    Delete get_font_name.py

--- get_font_name.py
+++
@@ -1,6 +0,0 @@
-import os
-from fontTools import ttLib
-
-file = os.path.abspath("./resource/src/font/NotoSansKR-SemiBold.ttf")
-with open(file, 'r') as f:
-    print(f.readlines(encodings='cp949'))
